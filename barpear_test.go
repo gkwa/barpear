b6bcb39ab37b5253693b0b7c55f44935fb43b097
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-11 15:58:47 -0800

    Add tests

---
+++ barpear_test.go
@@ -0,0 +1,85 @@
+package barpear
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestRandomPositiveIntegerSliceUpToMax_WithSeed(t *testing.T) {
+	tests := []struct {
+		name     string
+		max      int
+		seed     int64
+		expected []int
+	}{
+		{
+			name:     "Seed42",
+			max:      5,
+			seed:     int64(42),
+			expected: []int{4, 1, 3, 0, 2, 5},
+		},
+		{
+			name:     "seedTime",
+			max:      5,
+			seed:     int64(43),
+			expected: []int{4, 2, 1, 0, 5, 3},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			randomSlice := RandomPositiveIntegerSliceUpToMax(tt.max, WithSeed(tt.seed))
+
+			// Print debug information only when running with -v flag
+			if testing.Verbose() {
+				fmt.Println(randomSlice)
+			}
+
+			// Check if the length of the generated slice is as expected
+			if len(randomSlice) != tt.max+1 {
+				t.Errorf("Unexpected length of the generated slice. Expected: %d, Got: %d", tt.max+1, len(randomSlice))
+			}
+
+			// Check if the generated slice contains all positive integers up to max
+			for i := 0; i <= tt.max; i++ {
+				found := false
+				for _, num := range randomSlice {
+					if num == i {
+						found = true
+						break
+					}
+				}
+				if !found {
+					t.Errorf("Generated slice is missing the expected value: %d", i)
+				}
+			}
+
+			// Check if the generated slice is shuffled by comparing with the expected slice
+			if !reflect.DeepEqual(randomSlice, tt.expected) {
+				t.Errorf("Generated slice is not as expected. Expected: %v, Got: %v", tt.expected, randomSlice)
+			}
+		})
+	}
+}
+
+func TestRandomPositiveIntegerSliceUpToMax_UniqueOrder(t *testing.T) {
+	max := 5
+	seed42 := int64(42)
+	seedTime := time.Now().UnixNano()
+
+	// Generate slices with different seeds
+	slice42 := RandomPositiveIntegerSliceUpToMax(max, WithSeed(seed42))
+	sliceSeededFromTime := RandomPositiveIntegerSliceUpToMax(max, WithSeed(seedTime))
+
+	if testing.Verbose() {
+		fmt.Println("   Slice with seed42:", slice42)
+		fmt.Println("Slice with time seed:", sliceSeededFromTime)
+	}
+
+	// Check if the generated slices have unique orders
+	if reflect.DeepEqual(slice42, sliceSeededFromTime) {
+		t.Errorf("Generated slices have the same order for different seeds. Seed42: %v, seedTime: %v", slice42, sliceSeededFromTime)
+	}
+}
